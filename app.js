210c979ab6fe29c07068a059a4e1a14547af2669
[haileymoon] <moongirl1148@naver.com>
2022-06-19 21:40:05 +0900

    router and routes

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 require('dotenv/config')
+//Import Routes
+const postsRoute = require('./routes/posts'); //routes폴더의 posts파일로 경로 설정
+// 아래 라인으로 인해 posts.js에서 /post를 빼도 됨
+app.use('/posts', postsRoute); // middleware, if /posts get hit, make sure to use the postRoute
+
 
 //ROUTES
 app.get('/',(req, res) => {
     res.send("we are on home");
 })
-
-app.get('/posts',(req, res)=>{
-    res.send("we are on posts");
-})
-
 
 //Connect to DB
 mongoose.connect(
